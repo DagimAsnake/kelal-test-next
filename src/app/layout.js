bdd34c9757248943b089c807681c86f8a3a54ff4
Dagim Asnake <dagimasnake1@gmail>
2024-02-11 09:28:51 +0300

    typo fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css';
-import Navbar from '@/components/navbar/navbar';
+import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
 import StoreProvider from '@/store/storeProvider';
 
